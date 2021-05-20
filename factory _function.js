c46ca9533d23ed7145809737604098345009c597
Ncebakazi <ncebakazi414@gmail.com>
2021-05-20 22:09:38 +0200

    added style

--- factory _function.js
+++ factory _function.js
@@ -1,34 +1,40 @@
-function registraion_numbers(){
+function registraion_numbers() {
     // var objC = {}
     // var objB = {}
     // var objP = {}
     var objreg = {}
-    function setReg(regs){
-        if(objreg[regs]===undefined){
-            obreg[regs] = 0;
+    function setReg(regs) {
+        if (objreg[regs] === undefined) {
+            objreg[regs] = 0;
+
         }
+
+
     }
-    function getReg(){
+    function getReg() {
         return objreg;
+    }
+    // function registration(){
+    //     var regex = "^(CA|CY|CL\s\d{3}\s\d{3})$"
+    // }
 
+    //    function regAndtown(reg,){
+    //        if(reg.startsWith("CA")){
+    //        reg = objC
+    //        }
+    //        if(reg.startsWith("CY")){
+    //            reg = objB
+    //     }
+    //     if(reg.startsWith("CJ")){
+    //         reg = objP
+    //     }
+
+    //    }
+    return {
+        //regAndtown,
+        setReg,
+        getReg,
+        //registration
     }
 
-//    function regAndtown(reg,){
-//        if(reg.startsWith("CA")){
-//        reg = objC
-//        }
-//        if(reg.startsWith("CY")){
-//            reg = objB
-//     }
-//     if(reg.startsWith("CJ")){
-//         reg = objP
-//     }
-       
-//    }
-   return{
-       //regAndtown,
-       setReg,
-       getReg,
-   }
-    
 }
--- registration_number.js
+++ registration_number.js
@@ -1,16 +1,50 @@
 var textarea = document.querySelector("#form")
 var radiobtn = document.querySelector(".reg")
 var addbtn = document.querySelector(".Addbtn")
-var regNum = document.querySelectorAll(".registrationNumbers")
+var regNum = document.querySelector(".registrationNumbers")
 
 var registrationN = registraion_numbers()
 
-function submitReg(){
+function registration(reg){
+  var element = document.createElement("li");                
+  var textnode = document.createTextNode(reg);     
+  element.appendChild(textnode);
+  element.classList.add('reg_number');
+                                
+  document.getElementById("myList").appendChild(element); 
+  
+
+}
+function addReg(){
+  registration(textarea.value)
+}
+
+addbtn.addEventListener('click',addReg)
+
+//function submitReg(){
+  // var registration = textarea.value
+  //  var h4= document.getElementById("reg")
+  //  var button = document.getElementById("btn")
+  //  var input =document.getElementById("form")
+
+  //  var element = document.createElement("h4");
+  // // var makeText  = document.createTextNode(registration);
+  //  element.innerHTML = registration
+
+  // add the text node to the newly created div
+  
     // var radiobutton = document.querySelector(".reg:checked")
-    var registration = textarea.value
+    // var registration = textarea.value
+    // registrationN.setReg(registration)
+    // console.log(registration)
     // if( radiobutton){
     //     var town = radiobutton.value
     // }
-    regNum.innerHtml = registrationN.getReg(registration)
-}
-addbtn.addEventListener('click',submitReg)
+    
+   // regNum.innerHTML = registrationN.getReg()
+
+//  button.addEventListener('click', function(){
+//    h4.appendChild(element)
+//   } , false);
+
+//addbtn.addEventListener('click', submitReg)

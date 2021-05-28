9ea784bf2b7bc30bbcbbe2a0e98af6bcdff95b64
Ncebakazi <ncebakazi414@gmail.com>
2021-05-28 08:43:22 +0200

    added changes

--- registration_number.js
+++ registration_number.js
@@ -4,7 +4,7 @@ var addbtn = document.querySelector(".Addbtn")
 var regNum = document.querySelector(".registrationNumbers")
 var clearbtn = document.querySelector(".clear")
 var displaybtn = document.querySelector(".display")
-var displayMessage = document.querySelector (".message")
+var displayMessage = document.querySelector(".message")
 var specifTownbtn = document.querySelector(".town")
 var listElem = document.querySelector("#myList")
 console.log(listElem)
@@ -12,129 +12,123 @@ console.log(listElem)
 function textclear() {
   textarea.value = ""
   document.getElementById('Cape').checked = false
-  document.getElementById('Bellville').checked =false
-  document.getElementById('Paarl').checked =false
+  document.getElementById('Bellville').checked = false
+  document.getElementById('Paarl').checked = false
 
 }
 var registrationList = JSON.parse(localStorage.getItem('registrations'))
 var registrationN = registration_numbers(registrationList)
- if (registrationList) {
+if (registrationList) {
   for (var i = 0; i < registrationList.length; i++) {
-    var x = registrationList[i] 
-  appendElement(x)
-   }
+    var x = registrationList[i]
+    appendElement(x)
+  }
 }
 
-function appendElement(newValue){
+function appendElement(newValue) {
   newValue = newValue.toUpperCase()
-      var element = document.createElement("li");
-      var textnode = document.createTextNode(newValue);
-     element.appendChild(textnode);
-     element.classList.add('reg_number');
-     document.getElementById("myList").appendChild(element); 
+  var element = document.createElement("li");
+  var textnode = document.createTextNode(newValue);
+  element.appendChild(textnode);
+  element.classList.add('reg_number');
+  document.getElementById("myList").appendChild(element);
 }
-function registration(){
+function registration() {
   var value = document.querySelector("#form").value
   registrationList = JSON.parse(localStorage.getItem('registrations'))
   registrationN.setReg(value)
-  // if(!regex.match(value)){
-  //   displayMessage.innerHTML = "Please follow this format CA 123-123/ CA 123 123"
-  //   displayMessage.classList.add("error")
-  //   return false
-  // }
-  if(value === ""){
-    setTimeout(function(){
+  if (value === "") {
+    setTimeout(function () {
       displayMessage.innerHTML = "Please enter registration number"
       displayMessage.classList.add("error")
-    },0);
-    setTimeout(function(){
+    }, 0);
+    setTimeout(function () {
       displayMessage.innerHTML = ""
-    },2000);
+    }, 2000);
     return false
-  } 
-  if(!registrationN.setReg(value)) {
-    setTimeout(function(){
+  }
+  if(registrationN.setReg(value)) {
+ 
+    if (registrationList) {
+      if (registrationN.isReapted(registrationList)) {
+        setTimeout(function () {
+          displayMessage.innerHTML = "This registration number already exist"
+          displayMessage.classList.add("error")
+        }, 0);
+        setTimeout(function () {
+          displayMessage.innerHTML = ""
+        }, 2000);
+
+      } else {
+        localStorage.setItem('registrations', JSON.stringify(registrationN.getReglist()));
+        appendElement(value)
+      }
+    } else {
+      localStorage.setItem('registrations', JSON.stringify(registrationN.getReglist()));
+      appendElement(value)
+    }
+    textclear()
+  }
+  else{
+      setTimeout(function(){
       displayMessage.innerHTML = "Please follow this format CA 123-123/ CA 123 123"
       displayMessage.classList.add("error")
      
     },0);
     setTimeout(function(){
       displayMessage.innerHTML = ""
-    }, 2000);
-    return false
-  } 
-  else{
-  if(registrationList){
-    if(registrationN.isReapted(registrationList)){
-      setTimeout(function(){
-        displayMessage.innerHTML = "This registration number already exist"
-      displayMessage.classList.add("error")
-      }, 0);
-      setTimeout(function() {
-        displayMessage.innerHTML = ""
-      }, 2000);
-       
-    } else{
-      localStorage.setItem('registrations', JSON.stringify(registrationN.getReglist()));
-      appendElement(value)
-    }
-  } else{
-    localStorage.setItem('registrations', JSON.stringify(registrationN.getReglist()));
-    appendElement(value)
+    }, 2000); 
   }
-  textclear()
 }
-  }
-  
 
-function forTown(){
+function forTown() {
   var radiobutton = document.querySelector(".reg:checked")
   var specificT = radiobutton.value
-  // if(specificT && registrationList===""){
-  //   displayMessage.innerHTML = "Currently there are no existing registration numbers"
-  //   displayMessage.classList.add("error") 
-  // }
-  for(var i =0; i<registrationList.length;i++){
-    if(registrationList[i].startsWith(specificT)){
-      document.getElementById("myList").innerHTML = ""
+  document.getElementById("myList").innerHTML = ""
+
+  for (var i = 0; i < registrationList.length; i++){
+    if (registrationList[i].startsWith(specificT)) {
       appendElement(registrationList[i])
     }
-     else{
-       setTimeout(function(){
-      displayMessage.innerHTML = "There is no registration number for this town"
-      displayMessage.classList.add("error") 
-       }, 0);
-      setTimeout(function() {
+
+    else {
+      setTimeout(function () {
+        displayMessage.innerHTML = "There is no registration number for this town"
+        displayMessage.classList.add("error")
+      }, 0);
+      setTimeout(function () {
         displayMessage.innerHTML = ""
       }, 2000);
-     }
+      
+    }
+    
   }
   textclear()
-  
 }
-function displayAll(){
+function displayAll() {
+   document.getElementById("myList").innerHTML = ""
   if (registrationList) {
     for (var i = 0; i < registrationList.length; i++) {
-      var x = registrationList[i] 
-    appendElement(x)
-     }
+      var x = registrationList[i]
+      appendElement(x)
+    }
   }
-  else{
-    setTimeout(function(){
+  else {
+    setTimeout(function () {
       displayMessage.innerHTML = "Currently there are no existing registration numbers"
-      displayMessage.classList.add("error") 
-    },0);
-   setTimeout(function(){
-    displayMessage.innerHTML = ""
-    
-   },2000);
-  } 
+      displayMessage.classList.add("error")
+    }, 0);
+    setTimeout(function () {
+      displayMessage.innerHTML = ""
+
+    }, 2000);
   }
+}
 
 
 addbtn.addEventListener('click', registration)
-specifTownbtn.addEventListener('click',forTown)
-displaybtn.addEventListener('click' , displayAll)
+specifTownbtn.addEventListener('click', forTown)
+displaybtn.addEventListener('click', displayAll)
 
 clearbtn.addEventListener('click', function () {
   localStorage.clear()
@@ -143,28 +137,5 @@ clearbtn.addEventListener('click', function () {
 
 
 
-// function setlocalstorage(key, value) {
-//   window.setItem(key, JSON.stringify(value));
-// }
 
-// function getlocalstorage(key) {
-//   var value = window.localStorage.getItem(key);
-//   if (value == null) {
-//     return null
-//   }
-//   return JSON.parse(value)
-// }
 
-    // var radiobutton = document.querySelector(".reg:checked")
-    // var registration = textarea.value
-    // registrationN.setReg(registration)
-    // console.log(registration)
-    // if( radiobutton){
-    //     var town = radiobutton.value
-    // }
-
-   // regNum.innerHTML = registrationN.getReg()
-
-//  button.addEventListener('click', function(){
-//    h4.appendChild(element)
-//   } , false);
--- test/registration.test.js
+++ test/registration.test.js
@@ -1,14 +1,55 @@
 describe("The registration_numbers factory function", function () {
-    it("should show the registration under CapeTown if it starts with CA ", function () {
+    it("should return true for Cape Town if the registration starts with CA ", function () {
       let reg = registration_numbers();
-   
     assert.equal(reg.setReg("CA 123 123"), true);
-
-    });
-    it("It should return false if it contains other special character other than - ", function () {
+});
+  it("should return true for Bellville if the registration starts with CY  ", function () {
+  let reg = registration_numbers();
+  assert.equal(reg.setReg("CY 124 127"), true);
+});
+it("should return true for Paarl  if the registration starts with CL ", function () {
+  let reg = registration_numbers();
+assert.equal(reg.setReg("CL 123 123"), true);
+});
+describe("Registration numbers that are not from Cape Town,Bellville or Paarl", function (){
+it("should return false if the registration starts with CJ ", function () {
+  let reg = registration_numbers();
+assert.equal(reg.setReg("CJ 123 123"), false);
+});
+it("should return false if the registration starts with CF ", function () {
+  let reg = registration_numbers();
+assert.equal(reg.setReg("CF 335 474"), false);
+});
+it("should return false if the registration starts with GP ", function () {
+  let reg = registration_numbers();
+assert.equal(reg.setReg("CP 246 987"), false);
+});
+});
+describe("Format of registration numbers ", function (){
+    it("It should return false if the registration number contains other special character other than -", function () {
       let reg = registration_numbers();
-   
     assert.equal(reg.setReg("CA 123*123"), false);
 
     });
+    it("It should return true if the registration number contain - ", function () {
+      let reg = registration_numbers();
+    assert.equal(reg.setReg("CA 123-123"), true);
+
+    });
+    it("It should return true if the registration number contain space", function () {
+      let reg = registration_numbers();
+    assert.equal(reg.setReg("CA 123-123"), true);
+
+    });
+    it("It should return false if the registration number does not contain space", function () {
+      let reg = registration_numbers();
+    assert.equal(reg.setReg("CA 123-123"), true);
+
+    });
+    it("It should return false if the registration number is more than 6 numbers", function () {
+      let reg = registration_numbers();
+    assert.equal(reg.setReg("CA 123-1237"), false);
+
+    });
+  });
 });
